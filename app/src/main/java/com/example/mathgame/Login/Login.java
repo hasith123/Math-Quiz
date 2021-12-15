b871d28bf80a0e7001ff6271dcc1114d13e36d01
hasith bandara <hasith.bandara31@gmail.com>
2021-12-15 09:13:46 +0530

    Player IP address

--- app/src/main/java/com/example/mathgame/Login/Login.java
+++ app/src/main/java/com/example/mathgame/Login/Login.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -22,23 +25,33 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Login extends AppCompatActivity {
 
     ImageView imageTop;
-    TextView logo , slogan, ip_address;
-    Button btnLogin , newUser;
-    TextInputLayout txt_login_username , txt_login_password;
+    TextView logo, slogan, ip_address;
+    Button btnLogin, newUser;
+    TextInputLayout txt_login_username, txt_login_password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
         logo = findViewById(R.id.logo_name);
-        slogan =findViewById(R.id.slogan_name);
+        slogan = findViewById(R.id.slogan_name);
+        ip_address = findViewById(R.id.ip_address);
         btnLogin = findViewById(R.id.btn_login);
         newUser = findViewById(R.id.btn_newUser);
         txt_login_username = findViewById(R.id.txt_login_username);
         txt_login_password = findViewById(R.id.txt_login_password);
+
 
         //Setting up the new user button
         newUser.setOnClickListener(new View.OnClickListener() {
@@ -50,10 +63,12 @@ public class Login extends AppCompatActivity {
 
             }
         });
+
+
     }
 
 
-    //Validations
+    //Validate username
     public Boolean checkUsername() {
         String value = txt_login_username.getEditText().getText().toString();
 
@@ -68,6 +83,7 @@ public class Login extends AppCompatActivity {
         }
     }
 
+    //Validate password
     public Boolean checkPassword() {
         String value = txt_login_password.getEditText().getText().toString();
 
@@ -123,7 +139,7 @@ public class Login extends AppCompatActivity {
                         String phoneFromDatabase = snapshot.child(userGivenUsername).child("phone").getValue(String.class);
                         String usernameFromDatabase = snapshot.child(userGivenUsername).child("userName").getValue(String.class);
 
-                        //IP_Load();
+                        IP_Load();
 
                         Intent intent = new Intent(Login.this, MainActivity.class);
                         startActivity(intent);
@@ -131,9 +147,7 @@ public class Login extends AppCompatActivity {
                         txt_login_password.setError("Incorrect Password!");
                         txt_login_password.requestFocus();
                     }
-                }
-
-                else {
+                } else {
                     txt_login_username.setError("There is no such a user!");
                     txt_login_username.requestFocus();
                 }
@@ -146,9 +160,29 @@ public class Login extends AppCompatActivity {
         });
     }
 
-    //private void IP_Load() {
-        //ip_address = (TextView) findViewById(R.id.ip_address);
-        //WifiManager wifiManager = (WifiManager)getApplicationContext().getSystemService(WIFI_SERVICE);
-        //ip_address.setText("Player IP Address is "+ Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress()));
-    //}
+
+    //Show the ip address of the player
+    private void IP_Load() {
+//        ip_address = (TextView) findViewById(R.id.ip_address);
+//        WifiManager wifiManager = (WifiManager)getApplicationContext().getSystemService(WIFI_SERVICE);
+//        ip_address.setText("Player IP Address is "+ Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress()));
+
+
+
+        try{
+            StrictMode.ThreadPolicy threadpolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(threadpolicy);
+            URL url = new URL("http://checkip.amazonaws.com/");
+            URLConnection  urlConnection = url.openConnection();
+            urlConnection.setConnectTimeout(1000);
+            urlConnection.setReadTimeout(1000);
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            ip_address.setText("IP is "+bufferedReader.readLine());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
